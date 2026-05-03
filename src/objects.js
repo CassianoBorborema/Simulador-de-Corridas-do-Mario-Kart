d803dba3aa13224708321a0e18297108e12dfb45
CassianoBorborema <ladeiacassiano@gmail.com>
2026-05-03 20:34:37 -0300

    Objects Done

---
+++ src/objects.js
@@ -0,0 +1,15 @@
+const player1 = {
+  name: "Mario",
+  velocity: 4,
+  manobra: 3,
+  power: 3,
+  pontos: 0,
+};
+
+const player2 = {
+  name: "Luigi",
+  velocity: 3,
+  manobra: 4,
+  power: 4,
+  pontos: 0,
+};
